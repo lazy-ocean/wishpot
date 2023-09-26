e77774c89d4f9a15d901a52a72406cf6205cf870
Vladlena Panchenko <57011546+lazy-ocean@users.noreply.github.com>
2023-09-26 19:20:21 +0200

    fix: add header logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -9,19 +9,23 @@ import { SmallHeading, RegularText } from "../../theme/typography";
 import { ReactElement } from "react";
 import { Button } from "../Button/Button";
 import Link from "next/link";
+import Image from "next/image";
 
 export const Header = ({ children }: { children: ReactElement }) => {
   const { user } = useUser();
 
   return (
     <HeaderContainer>
+      <Link href={"/"}>
+        <Image src={"/favicon.ico"} alt="Wishpot" width={60} height={60} />
+      </Link>
+
       {children}
       {user && (
         <UserPanel>
           <Button>
             <Link href="/api/auth/logout">Logout</Link>
           </Button>
-
           <UserIcon src={user.picture} alt={user.name} width={40} height={40} />
           <UserData>
             <SmallHeading>{user.nickname}</SmallHeading>
--- src/components/ThemeSwitcher/ThemeSwitcher.styled.tsx
+++ src/components/ThemeSwitcher/ThemeSwitcher.styled.tsx
@@ -2,6 +2,7 @@ import { Classic } from "@theme-toggles/react";
 import styled from "styled-components";
 
 export const Switcher = styled(Classic)`
+  margin-left: auto;
   color: ${({ theme }) => theme.palette.font};
   svg {
     width: 3rem;
